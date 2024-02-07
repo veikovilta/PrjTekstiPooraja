1c2afb566ca45126bd794387413a66067bf4e87b
vevilt <vevilt@ICT-50120K>
2024-02-07 16:52:58 +0200

    base form design done

--- PrjKonteiner/formKasutajaAken.Designer.vb
+++ PrjKonteiner/formKasutajaAken.Designer.vb
@@ -22,17 +22,108 @@ Partial Class formKasutajaAken
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtSisendTekst = New System.Windows.Forms.TextBox()
+        Me.txtValjundTekst1 = New System.Windows.Forms.TextBox()
+        Me.txtValjundTekst2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnPoora1 = New System.Windows.Forms.Button()
+        Me.btnPoora2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'txtSisendTekst
+        '
+        Me.txtSisendTekst.Location = New System.Drawing.Point(297, 104)
+        Me.txtSisendTekst.Name = "txtSisendTekst"
+        Me.txtSisendTekst.Size = New System.Drawing.Size(303, 26)
+        Me.txtSisendTekst.TabIndex = 0
+        '
+        'txtValjundTekst1
+        '
+        Me.txtValjundTekst1.Location = New System.Drawing.Point(297, 182)
+        Me.txtValjundTekst1.Name = "txtValjundTekst1"
+        Me.txtValjundTekst1.Size = New System.Drawing.Size(303, 26)
+        Me.txtValjundTekst1.TabIndex = 1
+        '
+        'txtValjundTekst2
+        '
+        Me.txtValjundTekst2.Location = New System.Drawing.Point(297, 270)
+        Me.txtValjundTekst2.Name = "txtValjundTekst2"
+        Me.txtValjundTekst2.Size = New System.Drawing.Size(303, 26)
+        Me.txtValjundTekst2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(68, 109)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Sisendtekst"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(68, 188)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(175, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Pöörmine funktsiooniga"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(68, 276)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(157, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Pööra protseduuriga "
+        '
+        'btnPoora1
+        '
+        Me.btnPoora1.Location = New System.Drawing.Point(297, 214)
+        Me.btnPoora1.Name = "btnPoora1"
+        Me.btnPoora1.Size = New System.Drawing.Size(199, 50)
+        Me.btnPoora1.TabIndex = 6
+        Me.btnPoora1.Text = "Pööra funktsiooniga "
+        Me.btnPoora1.UseVisualStyleBackColor = True
+        '
+        'btnPoora2
+        '
+        Me.btnPoora2.Location = New System.Drawing.Point(297, 302)
+        Me.btnPoora2.Name = "btnPoora2"
+        Me.btnPoora2.Size = New System.Drawing.Size(199, 50)
+        Me.btnPoora2.TabIndex = 7
+        Me.btnPoora2.Text = "Pööra protseduuriga"
+        Me.btnPoora2.UseVisualStyleBackColor = True
         '
         'formKasutajaAken
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnPoora2)
+        Me.Controls.Add(Me.btnPoora1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtValjundTekst2)
+        Me.Controls.Add(Me.txtValjundTekst1)
+        Me.Controls.Add(Me.txtSisendTekst)
         Me.Name = "formKasutajaAken"
         Me.Text = "Tekstitöötlus "
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents txtSisendTekst As TextBox
+    Friend WithEvents txtValjundTekst1 As TextBox
+    Friend WithEvents txtValjundTekst2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnPoora1 As Button
+    Friend WithEvents btnPoora2 As Button
 End Class
--- PrjKonteiner/formKasutajaAken.vb
+++ PrjKonteiner/formKasutajaAken.vb
@@ -2,4 +2,8 @@
     Private Sub formKasutajaAken_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub txtSisendTekst_TextChanged(sender As Object, e As EventArgs) Handles txtSisendTekst.TextChanged
+
+    End Sub
 End Class
